301c3c1bedbad4c0e3e04da50e06c19592497d66
jayp-gh <dear.jayp@gmail.com>
2022-07-08 00:28:54 +0900

    명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,5 +3,20 @@ package com.ll.exam;
 public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
+
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while(true) {
+            System.out.printf("명령) ");
+           령String cmd = sc.nextLine().trim();
+
+            switch (cmd) {
+                case "종료":
+                    break outer;
+
+            }
+        }
+        sc.close();
     }
 }
