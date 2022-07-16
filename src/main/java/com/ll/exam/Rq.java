77a73c7e961362cb2c475afaa1ee13f23cea8e4c
jayp-gh <dear.jayp@gmail.com>
2022-07-16 17:52:30 +0900

    Rq.getPath 메서드 구현

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -23,6 +23,11 @@ public class Rq {
 
         return defaultValue;
     }
+
+    public String getPath() {
+        String[] urlBits = url.split("\\?", 2);
+
+        return urlBits[0];
 }
         }
     }
